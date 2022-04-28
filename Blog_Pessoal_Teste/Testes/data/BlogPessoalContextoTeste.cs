36b396c14ff3dea9b325b6f48d7db60a484c2b2c
unknown <leonardocasinha12@gmail.com>
2022-04-28 16:59:34 -0300

    @Leonardo-SARTO-Conselheiro - [Task: Teste de Contexto] {28/04/2022 17:00}

---
+++ Blog_Pessoal_Teste/Testes/data/BlogPessoalContextoTeste.cs
@@ -0,0 +1,43 @@
+using Blog_Pessoal.src.data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.EntityFrameworkCore;
+using Blog_Pessoal.src.modelos;
+using System.Linq;
+
+namespace Blog_Pessoal_Teste.Testes.data
+{
+    [TestClass]
+    public class BlogPessoalContextoTeste
+    {
+        private BlogPessoalContexto _contexto;
+
+        [TestInitialize]
+        public void inicio()
+        {
+            var opt = new DbContextOptionsBuilder<BlogPessoalContexto>()
+                .UseInMemoryDatabase(databaseName: "db_blogpessoal")
+                .Options;
+
+            _contexto = new BlogPessoalContexto(opt);
+        }
+
+        [TestMethod]
+
+        public void InserirNovoUsuarioNoBancoRetornaUsuario()
+        {
+            UsuarioModelo usuario = new UsuarioModelo();
+
+            usuario.Nome = "Leonardo Sarto";
+            usuario.Email = "leo@email.com";
+            usuario.Senha = "12345";
+            usuario.Foto = "linkdafoto";
+
+            _contexto.Usuarios.Add(usuario); // Adicionando usuario
+
+            _contexto.SaveChanges(); // Salvando o contexto (usuario)
+
+            Assert.IsNotNull(_contexto.Usuarios.FirstOrDefault(u => u.Email == "leo@email.com"));
+            
+        }
+    }
+}
