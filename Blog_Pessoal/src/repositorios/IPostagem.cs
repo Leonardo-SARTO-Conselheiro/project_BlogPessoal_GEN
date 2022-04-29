36133eaec646647610f4296c2ed20a6e70856476
unknown <leonardocasinha12@gmail.com>
2022-04-29 17:11:25 -0300

    @Leonardo-SARTO-Conselheiro - [Task: Criação dos DTOs e repositorio] {29/04/2022 17:10}

---
+++ Blog_Pessoal/src/repositorios/IPostagem.cs
@@ -0,0 +1,24 @@
+﻿using Blog_Pessoal.src.dtos;
+using Blog_Pessoal.src.modelos;
+using System.Collections.Generic;
+
+namespace Blog_Pessoal.src.repositorios
+{
+    /// <summary>
+    /// <para>Resumo: Responsavel por representar ações de CRUD de postagem</para>
+    /// <para>Criado por: Leonardo Sarto</para>
+    /// <para>Versão: 1.0</para>
+    /// <para>Data: 29/04/2022</para>
+    /// </summary>
+    public interface IPostagem
+    {
+        void NovaPostagem(NovaPostagemDTO postagem);
+        void AtualizarPostagem(AtualizarPostagemDTO postagem);
+        void DeletarPostagem(int id);
+        PostagemModelo PegarPostagemPeloId(int id);
+        List<PostagemModelo> PegarTodasPostagens();
+        List<PostagemModelo> PegarPostagensPeloTitulo(string titulo);
+        List<PostagemModelo> PegarPostagensPelaDescricao(string descricao);
+    }
+
+}
---
+++ Blog_Pessoal/src/repositorios/ITema.cs
@@ -0,0 +1,21 @@
+﻿using Blog_Pessoal.src.dtos;
+using Blog_Pessoal.src.modelos;
+using System.Collections.Generic;
+
+namespace Blog_Pessoal.src.repositorios
+{
+    /// <summary>
+    /// <para>Resumo: Responsavel por representar ações de CRUD de tema</para>
+    /// <para>Criado por: Leonardo Sarto</para>
+    /// <para>Versão: 1.0</para>
+    /// <para>Data: 29/04/2022</para>
+    /// </summary>
+    public interface ITema
+    {
+        void NovoTema(NovoTemaDTO tema);
+        void AtualizarTema(AtualizarTemaDTO tema);
+        void DeletarTema(int id);
+        TemaModelo PegarTemaPeloId(int id);
+        List<TemaModelo> PegarTemaPelaDescricao(string descricao);
+    }
+}
