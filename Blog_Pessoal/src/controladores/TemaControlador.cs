91a3b83a84b3a43beea27ed7b18c2672e5ea5919
unknown <leonardocasinha12@gmail.com>
2022-05-16 23:09:54 -0300

    @Leonardo-SARTO-Conselheiro - [Task: Adicionando Documentação do Tema] {16/05/2022 23:09}

--- Blog_Pessoal/src/controladores/TemaControlador.cs
+++ Blog_Pessoal/src/controladores/TemaControlador.cs
@@ -1,7 +1,9 @@
 ﻿using System.Threading.Tasks;
 using Blog_Pessoal.src.dtos;
+using Blog_Pessoal.src.modelos;
 using Blog_Pessoal.src.repositorios;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog_Pessoal.src.controladores
@@ -29,7 +31,14 @@ namespace Blog_Pessoal.src.controladores
 
 
         #region Métodos
-
+        /// <summary>
+        /// Pegar todos os Temas 
+        /// </summary>
+        /// <returns>ActionResult</returns>
+        /// <response code = "200">Retorna uma lista com todos o temas</response>
+        /// <response code = "204">Não existe nenhum tema</response>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         [HttpGet]
         [Authorize]
         public async Task<ActionResult> PegarTodosTemasAsync()
@@ -41,6 +50,15 @@ namespace Blog_Pessoal.src.controladores
             return Ok(lista);
         }
 
+        /// <summary>
+        /// Pegar Tema pelo Id
+        /// </summary>
+        /// <param name="idTema"></param>
+        /// <returns>ActionResult</returns>
+        /// <response code = "200">Retorna o tema</response>
+        /// <response code = "404">Tema não existe</response>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(TemaModelo))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet("id/{idTema}")]
         [Authorize]
         public async Task<ActionResult> PegarTemaPeloIdAsync([FromRoute] int idTema)
@@ -52,6 +70,15 @@ namespace Blog_Pessoal.src.controladores
             return Ok(tema);
         }
 
+        /// <summary>
+        /// Pegar Tema pela Descrição
+        /// </summary>
+        /// <param name="descricaoTema"></param>
+        /// <returns>ActionResult</returns>
+        /// <response code = "200">Retorna uma lista de temas</response>
+        /// <response code = "204">Tema não existe</response>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(TemaModelo))]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         [HttpGet("pesquisa")]
         [Authorize]
         public async Task<ActionResult> PegarTemasPelaDescricaoAsync([FromQuery] string descricaoTema)
@@ -63,6 +90,24 @@ namespace Blog_Pessoal.src.controladores
             return Ok(temas);
         }
 
+        /// <summary>
+        /// Criar novo Tema
+        /// </summary>
+        /// <param name="tema">NovoUsuarioDTO</param>
+        /// <returns>ActionResult</returns>
+        /// <remarks>
+        /// Exemplo de requisição:
+        ///
+        ///     POST /api/Temas
+        ///     {
+        ///        "Descricao": São Paulo Futebol Clube
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="201">Retorna tema criado</response>
+        /// <response code="400">Erro na requisição</response>
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(TemaModelo))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPost]
         [Authorize]
         public async Task<ActionResult> NovoTemaAsync([FromBody] NovoTemaDTO tema)
@@ -74,6 +119,25 @@ namespace Blog_Pessoal.src.controladores
             return Created($"api/Temas", tema);
         }
 
+        /// <summary>
+        /// Atualizar Tema
+        /// </summary>
+        /// <param name="tema">AtualizarUsuarioDTO</param>
+        /// <returns>ActionResult</returns>
+        /// <remarks>
+        /// Exemplo de requisição:
+        ///
+        ///     PUT /api/Temas
+        ///     {
+        ///        "id": 1,    
+        ///        "descricao": Libertadores da América
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="200">Retorna tema atualizado</response>
+        /// <response code="400">Erro na requisição</response>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(TemaModelo))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPut]
         [Authorize(Roles = "ADMINISTRADOR")]
         public async Task<ActionResult> AtualizarTema([FromBody] AtualizarTemaDTO tema)
@@ -85,6 +149,13 @@ namespace Blog_Pessoal.src.controladores
             return Ok(tema);
         }
 
+        /// <summary>
+        /// Deletar Tema pelo Id
+        /// </summary>
+        /// <param name="idTema">int</param>
+        /// <returns>ActionResult</returns>
+        /// <response code="204">Tema deletado</response>
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         [HttpDelete("deletar/{idTema}")]
         [Authorize(Roles = "ADMINISTRADOR")]
         public async Task<ActionResult> DeletarTema([FromRoute] int idTema)
--- Blog_Pessoal/src/repositorios/implementacoes/TemaRepositorio.cs
+++ Blog_Pessoal/src/repositorios/implementacoes/TemaRepositorio.cs
@@ -8,6 +8,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Blog_Pessoal.src.repositorios.implementacoes
 {
+    /// <summary>
+    /// <para>Resumo: Classe responsavel por implementar ITema</para>
+    /// <para>Criado por: Leonardo Sarto</para>
+    /// <para>Versão: 1.0</para>
+    /// <para>Data: 13/05/2022</para>
+    /// </summary>
     public class TemaRepositorio : ITema
     {
         #region Atributos
@@ -28,17 +34,30 @@ namespace Blog_Pessoal.src.repositorios.implementacoes
 
 
         #region Métodos
-
+        /// <summary>
+        /// <para>Resumo: Método assíncrono para pegar todos temas</para>
+        /// </summary>
+        /// <return>Lista TemaModelo</return>
         public async Task<List<TemaModelo>> PegarTodosTemasAsync()
         {
             return await _contexto.Temas.ToListAsync();
         }
 
+        /// <summary>
+        /// <para>Resumo: Método assíncrono para pegar um tema pelo Id</para>
+        /// </summary>
+        /// <param name="id">Id do tema</param>
+        /// <return>TemaModelo</return>
         public async Task<TemaModelo> PegarTemaPeloIdAsync(int id)
         {
             return await _contexto.Temas.FirstOrDefaultAsync(t => t.Id == id);
         }
 
+        /// <summary>
+        /// <para>Resumo: Método assíncrono para pegar temas pela descrição</para>
+        /// </summary>
+        /// <param name="descricao">Descrição do tema</param>
+        /// <return>Lista TemaModelo</return>
         public async Task<List<TemaModelo>> PegarTemasPelaDescricaoAsync(string descricao)
         {
             return await _contexto.Temas
@@ -46,6 +65,10 @@ namespace Blog_Pessoal.src.repositorios.implementacoes
                             .ToListAsync();
         }
 
+        /// <summary>
+        /// <para>Resumo: Método assíncrono para salvar um novo tema</para>
+        /// </summary>
+        /// <param name="tema">NovoTemaDTO</param>
         public async Task NovoTemaAsync(NovoTemaDTO tema)
         {
             await _contexto.Temas.AddAsync(new TemaModelo
@@ -56,6 +79,10 @@ namespace Blog_Pessoal.src.repositorios.implementacoes
             await _contexto.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// <para>Resumo: Método assíncrono para atualizar um tema</para>
+        /// </summary>
+        /// <param name="tema">AtualizarTemaDTO</param>
         public async Task AtualizarTemaAsync(AtualizarTemaDTO tema)
         {
             var temaExistente = await PegarTemaPeloIdAsync(tema.Id);
@@ -64,6 +91,10 @@ namespace Blog_Pessoal.src.repositorios.implementacoes
             await _contexto.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// <para>Resumo: Método assíncrono para deletar um tema</para>
+        /// </summary>
+        /// <param name="id">Id do tema</param>
         public async Task DeletarTemaAsync(int id)
         {
             _contexto.Temas.Remove(await PegarTemaPeloIdAsync(id));
